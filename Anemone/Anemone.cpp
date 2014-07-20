2533bfa5d3f0272b0644c6bb1e73fd3e753af5c5
eroha <eroha14@outlook.com>
2014-07-20 23:51:05 +0900

    설정창 수

--- Anemone/Anemone.cpp
+++ Anemone/Anemone.cpp
@@ -20,7 +20,7 @@ ATOM				MainWndClassRegister(HINSTANCE hInstance);
 ATOM				SettingClassRegister(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK	SettingProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	SettingProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(
@@ -164,7 +164,7 @@ ATOM SettingClassRegister(HINSTANCE hInstance)
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
 	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = SettingProc;
+	//wcex.lpfnWndProc = SettingProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
@@ -195,7 +195,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    int cx = GetSystemMetrics(SM_CXSCREEN);
    int cy = GetSystemMetrics(SM_CYSCREEN);
 
-   int x = 300;
+   int x = 500;
    int y = 200;
 
    hWnds.Main = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE | WS_EX_LAYERED | WS_EX_TOPMOST, szWindowClass, szTitle, WS_POPUP,
@@ -269,6 +269,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			break;
 
 		case IDM_WINDOW_SETTING:
+		{
 			if (IsWindow(hWnds.Setting) == false)
 			{
 				int cx = GetSystemMetrics(SM_CXSCREEN);
@@ -277,9 +278,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				int width = 630;
 				int height = 650;
 
+
+				hWnds.Setting = CreateDialog(hInst, MAKEINTRESOURCE(IDD_SETTING), hWnd, SettingProc);
+				/*
 				hWnds.Setting = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_NOACTIVATE, szSettingClass, NULL, WS_POPUP | WS_BORDER,
-					(cx - width) / 2, (cy - height) / 2, width, height,
-					hWnd, (HMENU)NULL, hInst, NULL);
+				(cx - width) / 2, (cy - height) / 2, width, height,
+				hWnd, (HMENU)NULL, hInst, NULL);
+				*/
+				SetWindowLong(hWnds.Setting, GWL_STYLE, WS_POPUP | WS_BORDER);
+				SetWindowLong(hWnds.Setting, GWL_EXSTYLE, WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_NOACTIVATE);
 				ShowWindow(hWnds.Setting, 1);
 			}
 			else
@@ -292,6 +299,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		break;
+	}
 	case WM_PAINT:
 	{
 		Cl.TextRenderer->Paint();
@@ -374,7 +382,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-LRESULT CALLBACK SettingProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK SettingProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 
@@ -405,8 +413,9 @@ LRESULT CALLBACK SettingProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPar
 		break;
 	case WM_ERASEBKGND:
 		return false;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
+	//default:
+	//	return DefWindowProc(hWnd, message, wParam, lParam);
+
 	}
 	return 0;
 }
